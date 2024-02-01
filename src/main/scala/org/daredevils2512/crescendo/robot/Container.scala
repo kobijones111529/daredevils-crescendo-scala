39f0ef86b777fe3e82a630d8b62d95e11116a0fb
Kobi Jones <kobijonesmail@gmail.com>
2024-01-31 20:50:12 -0600

    Format

--- src/main/scala/org/daredevils2512/crescendo/robot/Container.scala
+++ src/main/scala/org/daredevils2512/crescendo/robot/Container.scala
@@ -47,17 +47,16 @@ class Container:
   private def configureBindings(): Unit =
     for {
       drivetrain <- drivetrain
-    } yield
-      drivetrain.setDefaultCommand(
-        drivetrain.run(() =>
-          drivetrain.simpleDrive.foreach(drive =>
-            val move = MathUtil.applyDeadband(-xbox.getLeftY, 0.1)
-            val turn = MathUtil.applyDeadband(xbox.getLeftX, 0.1)
-            drive.arcadeDrive(move, turn)
-          )
+    } yield drivetrain.setDefaultCommand(
+      drivetrain.run(() =>
+        drivetrain.simpleDrive.foreach(drive =>
+          val move = MathUtil.applyDeadband(-xbox.getLeftY, 0.1)
+          val turn = MathUtil.applyDeadband(xbox.getLeftX, 0.1)
+          drive.arcadeDrive(move, turn)
         )
       )
-    end for
+    )
+
   end configureBindings
 
   def auto: Option[Command] = None
