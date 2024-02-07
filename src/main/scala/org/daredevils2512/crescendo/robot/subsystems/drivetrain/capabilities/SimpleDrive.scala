24b707b6487df5c5ddc862e7ea77c4c2c997491a
Kobi Jones <kobijonesmail@gmail.com>
2024-02-06 21:11:46 -0600

    Rename arcade drive args

--- src/main/scala/org/daredevils2512/crescendo/robot/subsystems/drivetrain/capabilities/SimpleDrive.scala
+++ src/main/scala/org/daredevils2512/crescendo/robot/subsystems/drivetrain/capabilities/SimpleDrive.scala
@@ -3,4 +3,4 @@ package org.daredevils2512.crescendo.robot.subsystems.drivetrain.capabilities
 trait SimpleDrive:
   def stop(): Unit
   def tank(left: Double, right: Double): Unit
-  def arcade(left: Double, right: Double): Unit
+  def arcade(move: Double, turn: Double): Unit
