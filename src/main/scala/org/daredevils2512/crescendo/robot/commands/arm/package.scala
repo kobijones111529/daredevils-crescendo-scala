70a8759cf854dc5d8777e4a01c67e88a434ebfcd
Kobi Jones <kobijonesmail@gmail.com>
2024-03-10 23:30:59 -0500

    Add actuate to position commands

--- src/main/scala/org/daredevils2512/crescendo/robot/commands/arm/package.scala
+++ src/main/scala/org/daredevils2512/crescendo/robot/commands/arm/package.scala
@@ -2,7 +2,10 @@ package org.daredevils2512.crescendo.robot.commands
 
 import edu.wpi.first.wpilibj2.command.Command
 import org.daredevils2512.crescendo.robot.subsystems.arm.Arm
-import org.daredevils2512.crescendo.robot.subsystems.arm.capabilities.SimpleActuate
+import org.daredevils2512.crescendo.robot.subsystems.arm.capabilities.{
+  Limit,
+  SimpleActuate
+}
 
 package object arm:
   def run(arm: Arm, simpleActuate: SimpleActuate, speed: => Double): Command =
@@ -10,3 +13,21 @@ package object arm:
       .run(() => simpleActuate.run(speed))
       .finallyDo(() => simpleActuate.stop())
   end run
+
+  def runToBottom(
+      arm: Arm,
+      simpleActuate: SimpleActuate,
+      bottomLimit: Limit,
+      speed: Double
+  ): Command =
+    run(arm, simpleActuate, -speed).until(() => bottomLimit.at)
+  end runToBottom
+
+  def runToTop(
+      arm: Arm,
+      simpleActuate: SimpleActuate,
+      topLimit: Limit,
+      speed: Double
+  ): Command =
+    run(arm, simpleActuate, speed).until(() => topLimit.at)
+  end runToTop
